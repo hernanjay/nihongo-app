2d3f386cec0ab80335777391cabf01abe44066bd
Hernan Jay Tiagan <hernan.tiagan@awsys-i.com>
2023-10-31 17:09:23 +0800

    Added navbar, login and register pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ColorModeScript } from '@chakra-ui/react'
 import App from './UI/Pages/Main/App.jsx'
+import theme from './UI/Pages/Main/theme.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </React.StrictMode>,
 )
