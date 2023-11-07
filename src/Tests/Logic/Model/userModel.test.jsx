deb73f882cc513b52bc36213aa857d84f9ccdf9a
Hernan Jay Tiagan <hernan.tiagan@awsys-i.com>
2023-11-07 15:05:11 +0800

    Added User Model For Testing

---
+++ src/Tests/Logic/Model/userModel.test.jsx
@@ -0,0 +1,17 @@
+import { describe, expect, it } from 'vitest';
+import User from '../../../Logic/Model/userModel';
+
+const user = new User("1", "admin", "admin", "admin@awsys-i.com");
+
+describe('Tests For User Model', () => {
+    it('User Model Username Getter/Setter Tests', () => {
+        expect(user.getUserName()).toBe("admin");
+        expect(user.setUserName('John')).toBe('John');
+    })
+    it('User Model Username Getter/Setter Validation Tests', () => {
+        expect(user.getUserName()).not.toBeNull();
+        expect(user.getUserName()).not.toBeUndefined();
+        expect(user.setUserName(true)).toBe(400);
+        expect(user.setUserName(1234)).toBe(400);
+    })
+})
