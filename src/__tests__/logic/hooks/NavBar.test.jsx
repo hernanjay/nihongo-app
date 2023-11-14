ace1445083c3c3903f7c72fd9cd10e231de2e768
[James Alwin Aljecera] <[james.aljecera@awsys-i.com]>
2023-11-14 13:03:10 +0800

    Alwin-added new test.jsx files and move to _tests_ folder

--- src/__tests__/logic/hooks/NavBar.test.jsx
+++ src/__tests__/logic/hooks/NavBar.test.jsx
@@ -1,43 +1,42 @@
 import React from "react";
 import { expect, it, describe } from "vitest";
-import { fireEvent, logRoles, render, screen } from "@testing-library/react";
-import NavBar from "../../../ui/Components/NavBar";
-import { AuthContextProvider } from "../../../logic/context/AuthContext";
+import { fireEvent, render, screen } from "@testing-library/react";
+import NavBar from "../../../ui/components/NavBar"
 import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "../../../logic/context/AuthContext";
 
 describe("Check if navbar renders properly", () => {
-  it("It should render", () => {
-    const { container } = render(
-      <BrowserRouter>
-        <AuthContextProvider>
-          <NavBar />
-        </AuthContextProvider>
-      </BrowserRouter>
-    );
-    logRoles(container);
+    it("It should render", () => {
+      render(
+        <BrowserRouter>
+          <AuthContextProvider>
+            <NavBar />
+          </AuthContextProvider>
+        </BrowserRouter>
+      );
+    });
+    it("Should Show a login button", () => {
+      render(
+        <BrowserRouter>
+          <AuthContextProvider>
+            <NavBar />
+          </AuthContextProvider>
+        </BrowserRouter>
+      );
+      const buttonElem = screen.getByText(/Login/i);
+      expect(buttonElem).toBeInTheDocument();
+    });
+    it("Should Login the user when login button is clicked", () => {
+       render(
+         <BrowserRouter>
+           <AuthContextProvider>
+             <NavBar />
+           </AuthContextProvider>
+         </BrowserRouter>
+       );
+       const buttonElem = screen.getByText(/Login/i);
+       expect(buttonElem).toBeInTheDocument();
+       fireEvent.click(buttonElem);
+       expect(buttonElem).toHaveBeenCalled();
+    });
   });
-  it("Should Show a login button", () => {
-    render(
-      <BrowserRouter>
-        <AuthContextProvider>
-          <NavBar />
-        </AuthContextProvider>
-      </BrowserRouter>
-    );
-    const buttonElem = screen.getByText(/Login/i);
-    expect(buttonElem).toBeInTheDocument();
-  });
-  it("Should Login the user when login button is clicked", () => {
-    render(
-      <BrowserRouter>
-        <AuthContextProvider>
-          <NavBar />
-        </AuthContextProvider>
-      </BrowserRouter>
-    );
-    const buttonElem = screen.getByText(/Login/i);
-    expect(buttonElem).toBeInTheDocument();
-    fireEvent.click(buttonElem);
-    expect(buttonElem).tohav;
-  });
-});
---
+++ src/__tests__/logic/hooks/login.test.jsx
@@ -0,0 +1,63 @@
+// import React from "react";
+import {describe,expect,it} from "vitest";
+// import {render,screen,logRoles} from "@testing-library/react"
+// import Login from '../../ui/pages/login/Login';
+// import { AuthContextProvider } from "../context/AuthContext";
+
+    const login = async (email, password) => {
+
+        const response = await fetch(
+            `https://aws-nihongo-api.onrender.com/api/users/login`,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    email,
+                    password,
+                }),
+            }
+        );
+
+        // const json = await response.json();
+
+        if (!response.ok) {
+            return response.status;
+        }
+
+        if (response.ok) {
+            return 200;
+        }
+    };
+
+
+
+describe("Login UT Cases v1", () => {
+    it("UT1-001 - User successfully login ( ✓ email ; ✓ password)", async () => {
+        const user = await login("nan@awsys-i.com","Wew@123321");
+        expect(user).toBe(200);
+    });
+    it("UT1-002 - Empty login credentials ((_) email ; (_) password)", async() => {
+        const user = await login(null,null);
+        expect(user).toBe(400);
+    });
+    it("UT1-003 - User not exist/Invalid login credential (✕ email ; ✕ password)", async() => {
+        const user = await login("james.aljecera@awsys-i.com","@Alje999");
+        expect(user).toBe(400);
+    });
+    it("UT1-004 - Empty password credential [password] (✓ email ; (_) password)", async() => {
+        const user = await login("nan@awsys-i.com",null);
+        expect(user).toBe(400);
+    });
+    it("UT1-005 - Empty email credential ((_) email ; ✓ password)", async() => {
+        const user = await login(null,"Wew@123321");
+        expect(user).toBe(400);
+    });
+    it("UT1-006 - Invalid password (✓ email ; ✕ password)", async() => {
+        const user = await login("nan@awsys-i.com","@Alje999");
+        expect(user).toBe(400);
+    });
+    it("UT1-007 - Invalid email (✕ email ; ✓ password)", async() => {
+        const user = await login("dene@gmail.com","Wew@123321");
+        expect(user).toBe(400);
+    });
+});
