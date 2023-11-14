7a8329bcf12977bc0ebe216af1fea3a4ac78d545
[James Alwin Aljecera] <[james.aljecera@awsys-i.com]>
2023-11-15 07:48:44 +0800

    Alwin - revisions for login and add another UT for signup

--- src/__tests__/logic/hooks/login.test.jsx
+++ src/__tests__/logic/hooks/login.test.jsx
@@ -35,29 +35,29 @@ describe("Login UT Cases v1", () => {
     it("UT1-001 - User successfully login ( ✓ email ; ✓ password)", async () => {
         const user = await login("nan@awsys-i.com","Wew@123321");
         expect(user).toBe(200);
-    }, 1000);
+    }, 500);
     it("UT1-002 - Empty login credentials ((_) email ; (_) password)", async() => {
         const user = await login(null,null);
         expect(user).toBe(400);
-    }, 1000);
+    }, 500);
     it("UT1-003 - User not exist/Invalid login credential (✕ email ; ✕ password)", async() => {
         const user = await login("james.aljecera@awsys-i.com","@Alje999");
         expect(user).toBe(400);
-    }, 1000);
+    }, 500);
     it("UT1-004 - Empty password credential [password] (✓ email ; (_) password)", async() => {
         const user = await login("nan@awsys-i.com",null);
         expect(user).toBe(400);
-    }, 1000);
+    }, 500);
     it("UT1-005 - Empty email credential ((_) email ; ✓ password)", async() => {
         const user = await login(null,"Wew@123321");
         expect(user).toBe(400);
-    }, 1000);
+    }, 500);
     it("UT1-006 - Invalid password (✓ email ; ✕ password)", async() => {
         const user = await login("nan@awsys-i.com","@Alje999");
         expect(user).toBe(400);
-    }, 1000);
+    }, 500);
     it("UT1-007 - Invalid email (✕ email ; ✓ password)", async() => {
         const user = await login("dene@gmail.com","Wew@123321");
         expect(user).toBe(400);
-    }, 1000);
+    }, 500);
 });
--- src/__tests__/logic/hooks/signup.test.jsx
+++ src/__tests__/logic/hooks/signup.test.jsx
@@ -31,40 +31,44 @@ describe("User Registration UT/IT v1", () => {
     it("UT2-001 - Empty one or more user's credentials (username: null,email: null, password: null, and/or verify password: null)", async () => {
         const user = await signup("junrel",null,null,null);
         expect(user).toBe(400);
-    },1000)
+    },500)
     it("UT2-002 - Invalid server provider (not @awsys-i.com) (email: _______@gmail.com / ______@yahoo.com)", async () => {
         const user = await signup("junrel","junrel@yahoo.com","Iam@1234","Iam@1234");
         expect(user).toBe(400);
-    },1000)
+    },500)
     it("UT2-003 - Not reached password's required length (password < required length)", async () => {
         const user = await signup("junrel","junrel@awsys-i.com","Iam@123","Iam@123");
         expect(user).toBe(400);
-    },1000)
+    },500)
     it("UT2-004 - Not contain at least one uppercase (password = alje@999)", async () => {
         const user = await signup("junrel","junrel@awsys-i.com","alje@999","alje@999");
         expect(user).toBe(400);
-    },1000)
+    },500)
     it("UT2-005 - Not contain at least one lowercade (password = ALJE@999)", async () => {
         const user = await signup("junrel","junrel@awsys-i.com","ALJE@999","ALJE@999");
         expect(user).toBe(400);
-    },1000)
+    },500)
     it("UT2-006 - Not contain at least one numerical figure (password = Alje@cera)", async () => {
         const user = await signup("junrel","junrel@awsys-i.com","Alje@cera","Alje@cera");
         expect(user).toBe(400);
-    },1000)
+    },500)
     it("UT2-007 - Not contain at least one special character (password = Alje9999)", async () => {
         const user = await signup("junrel","junrel@awsys-i.com","Alje9999","Alje9999");
         expect(user).toBe(400);
-    },1000)
-    it("UT2-008 - Password and Verify password did not match (password != verify password)", async () => {
+    },500)
+    it("UT2-008 - Contains Japanese characters (e.g , ひらがな、カタカナ、漢字)", async () => {
+        const user = await signup("junrel","junrel@awsys-i.com","大変だった@19","大変だった@19");
+        expect(user).toBe(400);
+    },500)
+    it("UT2-009 - Password and Verify password did not match (password != verify password)", async () => {
         const user = await signup("junrel","junrel@awsys-i.com","Alje9999","alje@999");
         expect(user).toBe(400);
-    },1000)
-    // it("UT2-009 - User's registered successfully", async () => {
+    },500)
+    // it("UT2-0010 - User's registered successfully", async () => {
     //     const user = await signup("junrel","jun@awsys-i.com","June@123","Jun@123");
     //     expect(user).toBe(200);
     // })
-    it("UT2-010 - User's record already exist", async () => {
+    it("UT2-011 - User's record already exist", async () => {
         const user = await signup("james.alwin","james.aljecera@awsys-i.com","Alje@999","Alje@999");
         expect(user).toBe(400);
     },1000)
