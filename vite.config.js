f51ce8e087fea5b2e2a760d303e6535a6fa74f23
Hernan Jay Tiagan <hernan.tiagan@awsys-i.com>
2023-11-15 12:57:54 +0800

    Fixed File Dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import "@testing-library/jest-dom";
 
 // https://vitejs.dev/config/
 export default defineConfig({
