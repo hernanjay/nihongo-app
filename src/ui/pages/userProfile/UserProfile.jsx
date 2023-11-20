4b51c41d4e2b4909657fd0a15227ddb41231cf6a
[gabrielpaderna] <[gabriel.paderna@awsys-i.com]>
2023-11-20 19:42:08 +0800

    Modified User Profile

--- src/ui/pages/userProfile/UserProfile.jsx
+++ src/ui/pages/userProfile/UserProfile.jsx
@@ -49,8 +49,45 @@ const UserProfile = () => {
             gap={4}
             bg={bg}
             >
-                <GridItem rowSpan={2} colSpan={8} />
-                <GridItem rowSpan={1} colSpan={2} />
+                <GridItem rowSpan={1} colSpan={8} />
+                <GridItem rowSpan={1} colSpan={3} />
+                <GridItem rowSpan={1} colSpan={3}>
+                    <Heading pb={4} size='md'>
+                        User Profile
+                    </Heading>
+                    <Stack direction='row'>
+                        <Stack>
+
+                            <FormControl>
+                                <FormLabel>First Name</FormLabel>
+                                <Input type='firstName' placeholder='First name' />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Birthday</FormLabel>
+                                <Input type='lastName' placeholder='Last name' />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Email</FormLabel>
+                                <Input type='cellNumber' placeholder='Cell number' />
+                            </FormControl>
+                        </Stack>
+                        <Divider orientation='vertical' />
+                        <Stack>
+                            <FormControl>
+                                <FormLabel>Last Name</FormLabel>
+                                <Input type='userName' placeholder='Username' />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Gender</FormLabel>
+                                <Input type='email' placeholder='Email' />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Phone</FormLabel>
+                                <Input type='email' placeholder='Email' />
+                            </FormControl>
+                        </Stack>
+                    </Stack>
+                </GridItem>
                 <GridItem rowSpan={1} colSpan={2}>
                     <Image
                         //objectFit='cover'
@@ -60,207 +97,48 @@ const UserProfile = () => {
                         boxSize="320px"
                     />
                 </GridItem>
-                <GridItem rowSpan={1} colSpan={2}>
+                <GridItem rowSpan={1} colSpan={3} />
+                <GridItem rowSpan={1} colSpan={3}>
                     <Heading pb={4} size='md'>
-                        User Profile
+                        Address
                     </Heading>
                     <Stack direction='row'>
                         <Stack>
 
                             <FormControl>
-                                <FormLabel>Firstname</FormLabel>
+                                <FormLabel>Address</FormLabel>
                                 <Input type='firstName' placeholder='First name' />
                             </FormControl>
                             <FormControl>
-                                <FormLabel>Lastname</FormLabel>
+                                <FormLabel>City</FormLabel>
                                 <Input type='lastName' placeholder='Last name' />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>Cell Number</FormLabel>
-                                <Input type='cellNumber' placeholder='Cell number' />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>Gender</FormLabel>
-                                <Input type='gender' placeholder='Gender' />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>Birthday</FormLabel>
-                                <Input type='birthday' placeholder='Birthday' />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>Address</FormLabel>
-                                <Input type='address' placeholder='Address' />
                             </FormControl>
                         </Stack>
                         <Divider orientation='vertical' />
                         <Stack>
                             <FormControl>
-                                <FormLabel>Username</FormLabel>
+                                <FormLabel>Number</FormLabel>
                                 <Input type='userName' placeholder='Username' />
                             </FormControl>
                             <FormControl>
-                                <FormLabel>Email</FormLabel>
+                                <FormLabel>Zip</FormLabel>
                                 <Input type='email' placeholder='Email' />
                             </FormControl>
                         </Stack>
                     </Stack>
+                </GridItem>
+                <GridItem rowSpan={1} colSpan={3} />
+                <GridItem rowSpan={1} colSpan={2}>
                     <Wrap spacing={4} pt={4}>
                         <WrapItem>
-                            <Button colorScheme='blue'>Edit Profile</Button>
+                            <Button colorScheme='blue'>Save All</Button>
                         </WrapItem>
-                        <WrapItem>
+                        {/* <WrapItem>
                             <Button colorScheme='red'>Cancel</Button>
-                        </WrapItem>
+                        </WrapItem> */}
                     </Wrap>
                 </GridItem>
-                <GridItem rowSpan={1} colSpan={2}></GridItem>
             </Grid>
-            /* <Card
-                direction={{ base: 'column', sm: 'row' }}
-                overflow='hidden'
-                bg={bg}
-                w="50vw"
-                variant="elevated"
-                size="sm"
-                boxShadow="lg"
-                px="5"
-                py="5"
-                m="10"
-            >
-                <Image
-                    //objectFit='cover'
-                    maxW={{ base: '100%', sm: '200px' }}
-                    src="src\assets\character_boy_normal.png"
-                    alt='pictureForUserProfile'
-                    boxSize="320px"
-                />
-                <Stack>
-                    <CardBody>
-                        <header>
-                            User Profile
-                        </header>
-                        <FormControl>
-                            <FormLabel>Firstname</FormLabel>
-                            <Input type='email' />
-                            <FormHelperText>We'll never share your email.</FormHelperText>
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel>Lastname</FormLabel>
-                            <Input type='email' />
-                            <FormHelperText>We'll never share your email.</FormHelperText>
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel>Cell Number</FormLabel>
-                            <Input type='email' />
-                            <FormHelperText>We'll never share your email.</FormHelperText>
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel>Gender</FormLabel>
-                            <Input type='email' />
-                            <FormHelperText>We'll never share your email.</FormHelperText>
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel>Birthday</FormLabel>
-                            <Input type='email' />
-                            <FormHelperText>We'll never share your email.</FormHelperText>
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel>Address</FormLabel>
-                            <Input type='email' />
-                            <FormHelperText>We'll never share your email.</FormHelperText>
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel>Username</FormLabel>
-                            <Input type='email' />
-                            <FormHelperText>We'll never share your email.</FormHelperText>
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel>Email</FormLabel>
-                            <Input type='email' />
-                            <FormHelperText>We'll never share your email.</FormHelperText>
-                        </FormControl>
-                        <Wrap spacing={4}>
-                            <WrapItem>
-                                <Button colorScheme='blue'>Edit Profile</Button>
-                            </WrapItem>
-                            <WrapItem>
-                                <Button colorScheme='red'>Cancel</Button>
-                            </WrapItem>
-                        </Wrap>
-                    </CardBody>
-                </Stack>
-            </Card> */
     )
-    /*
-    return (
-        <Grid
-            h="100vh"
-            templateRows="repeat(5, 1fr)"
-            templateColumns="repeat(8, 1fr)"
-            gap={4}
-            bg={bg}
-        >
-            <GridItem rowSpan={1} colSpan={8} />
-            <GridItem rowSpan={4} colSpan={1} />
-            
-            <GridItem rowSpan={1} colSpan={3}>
-                <Heading>User Profile</Heading>
-                <Text fontSize='md'>Name</Text>
-                <Editable defaultValue='John' bg='white' borderColor={"black"}>
-                    <EditablePreview />
-                    <EditableInput />
-                </Editable>
-                <Text fontSize='md'>Surname</Text>
-                <Editable defaultValue='Doe' bg='white' borderColor={"black"}>
-                    <EditablePreview />
-                    <EditableInput />
-                </Editable>
-            </GridItem>
-            <GridItem
-                rowSpan={1}
-                colSpan={3}
-                justifyContent="center"
-                alignContent="center"
-                >
-                <Image
-                    src="src\assets\kakedasu_suit1.png"
-                    boxSize="160px"
-                    alt="pictureForUserProfile"
-                ></Image>
-            </GridItem>
-            <GridItem rowSpan={1} colSpan={1} />
-            <GridItem rowSpan={1} colSpan={2}>
-                
-            </GridItem>
-            <GridItem rowSpan={1} colSpan={4}/>
-            <GridItem rowSpan={1} colSpan={1} />
-            <GridItem rowSpan={1} colSpan={1}>
-                <Text fontSize='md'>Birthday</Text>
-                <Editable defaultValue='01/01' bg='white' borderColor={"black"}>
-                    <EditablePreview />
-                    <EditableInput />
-                </Editable>
-                <Text fontSize='md'>Working since</Text>
-                <Editable defaultValue='07/28/2023' bg='white' borderColor={"black"}>
-                    <EditablePreview />
-                    <EditableInput />
-                </Editable>
-                <Text fontSize='md'>Department</Text>
-                <Editable defaultValue='Dev G' bg='white' borderColor={"black"}>
-                    <EditablePreview />
-                    <EditableInput />
-                </Editable>
-            </GridItem>
-            <GridItem colSpan={1} >
-                <Text fontSize='md'>Position</Text>
-                <Editable defaultValue='Junior R&D Engineer' bg='white' borderColor={"black"}>
-                    <EditablePreview />
-                    <EditableInput />
-                </Editable>
-            </GridItem>
-            <GridItem colSpan={4} />
-        </Grid>
-    );
-    */
 };
 export default UserProfile;
