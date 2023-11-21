47922229186ed50cc988e4ba34048adf235f64aa
karl <johnkarl.auxteriaco@awsys-i.com>
2023-11-21 13:01:16 +0800

    Restyling admin UI

--- src/ui/pages/admin/Admindashboard.jsx
+++ src/ui/pages/admin/Admindashboard.jsx
@@ -27,6 +27,7 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  GridItem,
 } from "@chakra-ui/react";
 import {
   FiHome,
@@ -40,6 +41,7 @@ import {
   FiSearch,
   FiBell,
 } from "react-icons/fi";
+// import Chart from "../../Components/Chart";
 // import Chart from "../../Components/chartComponent/Chart.jsx";
 // import { color } from "framer-motion";
 
@@ -47,7 +49,7 @@ export default function Admindashboard() {
   const [display, changeDisplay] = useState("hide");
   const boxStyle = {
     w: "100%",
-    h: "100",
+    h: "150",
     bgColor: "gray.400",
   };
   const headStyle = {
@@ -60,34 +62,43 @@ export default function Admindashboard() {
     textAlign: "center",
   };
   return (
-    <Box pt="12vh">
+    <Box>
       <Flex
         flexDir={["column", "column", "row"]}
         overflow="hidden"
-        maxW="2000px"
+        justifyContent="center"
+        boxShadow="outline"
       >
         {/* Column 1 */}
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="#">Home</BreadcrumbLink>
-          </BreadcrumbItem>
-
-          <BreadcrumbItem>
-            <BreadcrumbLink href="#">Docs</BreadcrumbLink>
-          </BreadcrumbItem>
-
-          <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink href="#">Breadcrumb</BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
         {/* Column 2 */}
-        <Flex w="100%" p="3%" flexDir="column" overflow="auto" minH="100vh">
-          {" "}
+        <Flex
+          w="90%"
+          p="5%"
+          flexDir="column"
+          overflow="auto"
+          minH="100vh"
+          bg="white"
+          pt="6%"
+        >
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">Docs</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem isCurrentPage>
+              <BreadcrumbLink href="#">Breadcrumb</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>{" "}
           <Heading
             fontWeight="normal"
             mb={4}
             letterSpacing="tight"
-            fontSize="2xl"
+            fontSize="5xl"
+            mt="2%"
           >
             Welcome,{" "}
             <Flex fontWeight="bold" display="inline-flex">
@@ -104,7 +115,7 @@ export default function Admindashboard() {
         </Text>{" "} */}
           {/* <Chart /> */}
           {/* fetch the id of student percentage per nihongo level */}
-          <Grid templateColumns="repeat(5, 1fr)" gap={6} pt="2%" wrap="wrap">
+          <Grid templateColumns="repeat(5, 1fr)" gap={6} mt="5%">
             <Box sx={boxStyle}>
               <Heading sx={headStyle} size="md">
                 N1
@@ -136,218 +147,17 @@ export default function Admindashboard() {
               <Text sx={textStyle}>Percentage Context</Text>
             </Box>
           </Grid>
-          <Flex justifyContent="space-between" mt={8}>
-            <Flex align="flex-end">
-              {/* <Heading as="h2" size="lg" letterSpacing="tight">
-              Lists
-            </Heading>
-            <Text fontSize="sm" color="gray" ml={4}>
-              November 2023
-            </Text> */}
-            </Flex>
-            {/* <IconButton icon={<FiCalendar />} /> */}
-            {/* CONTENT HERE */}
-          </Flex>
-          <Flex alignContent="center" p={5} m={10}>
-            <InputGroup
-              bgColor="#fff"
-              mb={4}
-              border="none"
-              borderColor="#fff"
-              borderRadius="10px"
-              mr={2}
-            >
-              <InputLeftElement
-                pointerEvents="none"
-                children={<FiSearch color="gray" />}
-              />
-              <Input type="text" placeholder="Search" borderRadius="10px" />
-            </InputGroup>
-            <IconButton
-              icon={<FiBell />}
-              fontSize="md"
-              bgColor="#fff"
-              borderRadius="50%"
-              p="10px"
-            />
-            <Flex
-              w={30}
-              h={25}
-              bgColor="#C53030"
-              borderRadius="50%"
-              color="#fff"
-              align="center"
-              justify="center"
-              ml="-3"
-              mt="-1"
-              zIndex="100"
-              fontSize="xs"
-            >
-              5
-            </Flex>
-          </Flex>
-          <Flex align="flex-end">
-            <Heading as="h2" size="lg" letterSpacing="tight">
-              List of Students
-            </Heading>
-            {/* <Text fontSize="sm" color="gray" ml={4}>
-              November 2023
-            </Text> */}
-          </Flex>
-          <Flex flexDir="column">
-            <Flex overflow="auto">
-              <Table variant="unstyled" mt={4}>
-                <Thead>
-                  <Tr color="gray">
-                    <Th>Name of Student</Th>
-                    <Th>Email</Th>
-                    <Th>Nihongo Level</Th>
-                    <Th isNumeric>Grade</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  <Tr>
-                    <Td>
-                      <Flex align="center">
-                        <Avatar size="sm" mr={2} src="" />
-                        <Flex flexDir="column">
-                          <Heading size="sm" letterSpacing="tight">
-                            Name of Student
-                          </Heading>
-                          {/* <Text fontSize="sm" color="gray">Name of Student</Text> */}
-                        </Flex>
-                      </Flex>
-                    </Td>
-                    <Td>Email</Td>
-                    <Td>Nihongo Level</Td>
-                    <Td isNumeric>
-                      <Text fontWeight="bold" display="inline-table">
-                        Grade
-                      </Text>
-                    </Td>
-                  </Tr>
-                  <Tr>
-                    <Td>
-                      <Flex align="center">
-                        <Avatar size="sm" mr={2} src="" />
-                        <Flex flexDir="column">
-                          <Heading size="sm" letterSpacing="tight">
-                            Name of Student
-                          </Heading>
-                          {/* <Text fontSize="sm" color="gray">Name of Student</Text> */}
-                        </Flex>
-                      </Flex>
-                    </Td>
-                    <Td>Email</Td>
-                    <Td>Nihongo Level</Td>
-                    <Td isNumeric>
-                      <Text fontWeight="bold" display="inline-table">
-                        Grade
-                      </Text>
-                    </Td>
-                  </Tr>
-                  <Tr>
-                    <Td>
-                      <Flex align="center">
-                        <Avatar size="sm" mr={2} src="" />
-                        <Flex flexDir="column">
-                          <Heading size="sm" letterSpacing="tight">
-                            Name of Student
-                          </Heading>
-                          {/* <Text fontSize="sm" color="gray">Name of Student</Text> */}
-                        </Flex>
-                      </Flex>
-                    </Td>
-                    <Td>Email</Td>
-                    <Td>Nihongo Level</Td>
-                    <Td isNumeric>
-                      <Text fontWeight="bold" display="inline-table">
-                        Grade
-                      </Text>
-                    </Td>
-                  </Tr>
-                  {display == "show" && (
-                    <>
-                      <Tr>
-                        <Td>
-                          <Flex align="center">
-                            <Avatar size="sm" mr={2} src="" />
-                            <Flex flexDir="column">
-                              <Heading size="sm" letterSpacing="tight">
-                                Name of Student
-                              </Heading>
-                              {/* <Text fontSize="sm" color="gray">Name of Student</Text> */}
-                            </Flex>
-                          </Flex>
-                        </Td>
-                        <Td>Email</Td>
-                        <Td>Nihongo Level</Td>
-                        <Td isNumeric>
-                          <Text fontWeight="bold" display="inline-table">
-                            Grade
-                          </Text>
-                        </Td>
-                      </Tr>
-                      <Tr>
-                        <Td>
-                          <Flex align="center">
-                            <Avatar size="sm" mr={2} src="" />
-                            <Flex flexDir="column">
-                              <Heading size="sm" letterSpacing="tight">
-                                Name of Student
-                              </Heading>
-                              {/* <Text fontSize="sm" color="gray">Name of Student</Text> */}
-                            </Flex>
-                          </Flex>
-                        </Td>
-                        <Td>Email</Td>
-                        <Td>Nihongo Level</Td>
-                        <Td isNumeric>
-                          <Text fontWeight="bold" display="inline-table">
-                            Grade
-                          </Text>
-                        </Td>
-                      </Tr>
-                      <Tr>
-                        <Td>
-                          <Flex align="center">
-                            <Avatar size="sm" mr={2} src="" />
-                            <Flex flexDir="column">
-                              <Heading size="sm" letterSpacing="tight">
-                                Name of Student
-                              </Heading>
-                              {/* <Text fontSize="sm" color="gray">Name of Student</Text> */}
-                            </Flex>
-                          </Flex>
-                        </Td>
-                        <Td>Email</Td>
-                        <Td>Nihongo Level</Td>
-                        <Td isNumeric>
-                          <Text fontWeight="bold" display="inline-table">
-                            Grade
-                          </Text>
-                        </Td>
-                      </Tr>
-                    </>
-                  )}
-                </Tbody>
-              </Table>
-            </Flex>
-            <Flex align="center">
-              <Divider />
-              <IconButton
-                icon={display == "show" ? <FiChevronUp /> : <FiChevronDown />}
-                onClick={() => {
-                  if (display == "show") {
-                    changeDisplay("none");
-                  } else {
-                    changeDisplay("show");
-                  }
-                }}
-              />
-              <Divider />
-            </Flex>
-          </Flex>
+          <Grid templateColumns="repeat(3, 1fr)" gap={10} pt="5%" h="60vh">
+            <GridItem bg={"blue"} colSpan={2}>
+              <Grid templateRows="repeat(3, 1fr)" h="52vh">
+                <GridItem rowSpan={1} w="100%" bg="blue"></GridItem>
+                <GridItem rowSpan={2} w="100%" bg="gray">
+                  {/* <Chart></Chart> */}
+                </GridItem>
+              </Grid>
+            </GridItem>
+            <GridItem bg={"green"}></GridItem>
+          </Grid>
         </Flex>
         {/* Column 3 */}
         {/* <Flex w="45%" bgColor="#f5f5f5" p="3%" flexDir="column" overflow="auto">
