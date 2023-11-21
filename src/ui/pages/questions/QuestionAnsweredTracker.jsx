c9c17033d3753859f926dad93d65255602e78997
Jayson Roda <jayson.roda@awsys-i.com>
2023-11-21 14:52:10 +0800

    Fix Question Page -Roda

--- src/ui/pages/questions/QuestionAnsweredTracker.jsx
+++ src/ui/pages/questions/QuestionAnsweredTracker.jsx
@@ -1,81 +1,92 @@
 import {
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Flex,
-  GridItem,
-  Tag,
-  TagLabel,
-  Text,
+    Button,
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    Flex,
+    GridItem,
+    Tag,
+    TagLabel,
+    Text,
 } from "@chakra-ui/react";
 import { useQuestionContext } from "../../../logic/hooks/question/useQuestionContext";
 
 const QuestionAnsweredTracker = ({
-  bg,
-  border,
-  checked,
-  hasSubmit,
-  setHasSubmit,
+    bg,
+    border,
+    checked,
+    hasSubmit,
+    setHasSubmit,
 }) => {
-  const { questions, answers } = useQuestionContext();
-  const allAnswered = answers.includes(null);
-  return (
-    <GridItem colSpan="1">
-      <Card
-        textColor={"black"}
-        boxShadow="lg"
-        bgColor={bg}
-        position="sticky"
-        top="7.5vw"
-        textAlign="center"
-      >
-        <CardHeader pb="-1">
-          <Text fontSize={"1.5vw"}>Answer Tracker</Text>
-        </CardHeader>
-        <CardBody>
-          <Flex flexWrap="wrap">
-            {questions?.map((question, index) => {
-              return (
-                <Tag
-                  key={question._id}
-                  size="lg"
-                  variant="outline"
-                  mx=".5rem"
-                  color={border}
-                  borderColor={border}
-                  mt="1rem"
-                  bg={
-                    hasSubmit
-                      ? checked[index]
-                        ? "green.300"
-                        : "red.300"
-                      : answers[index] !== null
-                      ? "blue.300"
-                      : bg
-                  }
-                >
-                  <TagLabel key={index}>{`Q${index + 1}`}</TagLabel>
-                </Tag>
-              );
-            })}
-          </Flex>
-        </CardBody>
-        <CardFooter justify={"end"}>
-          <Button
-            borderColor={border}
-            variant="outline"
-            isDisabled={allAnswered || hasSubmit}
-            onClick={() => {
-              setHasSubmit(true);
-            }}
-          >
-            Submit
-          </Button>
-        </CardFooter>
-      </Card>
-    </GridItem>
-  );
+    const { questions, answers } = useQuestionContext();
+    const allAnswered = answers.includes(null);
+    return (
+        <GridItem colSpan="1">
+            <Card
+                textColor={"black"}
+                boxShadow="lg"
+                bgColor={bg}
+                position="sticky"
+                top="7.5vw"
+                textAlign="center"
+            >
+                <CardHeader pb="-1">
+                    <Text fontSize={"1.5vw"}>Answer Tracker</Text>
+                </CardHeader>
+                <CardBody>
+                    <Flex flexWrap="wrap">
+                        {questions?.map((question, index) => {
+                            return (
+                                <Tag
+                                    key={question._id}
+                                    size="lg"
+                                    variant="outline"
+                                    mx=".5rem"
+                                    color={border}
+                                    borderColor={border}
+                                    mt="1rem"
+                                    bg={
+                                        hasSubmit
+                                            ? checked[index]
+                                                ? "green.300"
+                                                : "red.300"
+                                            : answers[index] !== null
+                                            ? "blue.300"
+                                            : bg
+                                    }
+                                >
+                                    <TagLabel key={index}>{`Q${
+                                        index + 1
+                                    }`}</TagLabel>
+                                </Tag>
+                            );
+                        })}
+                    </Flex>
+                </CardBody>
+                <CardFooter>
+                    <Button
+                        mx="auto"
+                        px="1.4rem"
+                        borderColor={border}
+                        variant="outline"
+                    >
+                        Clear
+                    </Button>
+                    <Button
+                        mx="auto"
+                        borderColor={border}
+                        variant="outline"
+                        isDisabled={allAnswered || hasSubmit}
+                        onClick={() => {
+                            setHasSubmit(true);
+                        }}
+                    >
+                        Submit
+                    </Button>
+                </CardFooter>
+            </Card>
+        </GridItem>
+    );
 };
 export default QuestionAnsweredTracker;
--- src/ui/pages/questions/QuestionList.jsx
+++ src/ui/pages/questions/QuestionList.jsx
@@ -3,25 +3,24 @@ import QuestionItem from "./QuestionItem";
 import { useQuestionContext } from "../../../logic/hooks/question/useQuestionContext";
 
 const QuestionList = ({ bg, hoverColor, hasSubmit }) => {
-  const { questions } = useQuestionContext();
-  return (
-    <GridItem colSpan={"2"}>
-      <Box maxW={"60vw"}>
-        <UnorderedList>
-          {console.log(questions)}
-          {questions?.map((qn, index) => (
-            <QuestionItem
-              qn={qn}
-              bg={bg}
-              hoverColor={hoverColor}
-              key={qn.question}
-              index={index}
-              hasSubmit={hasSubmit}
-            />
-          ))}
-        </UnorderedList>
-      </Box>
-    </GridItem>
-  );
+    const { questions } = useQuestionContext();
+    return (
+        <GridItem colSpan={"2"}>
+            <Box maxW={"60vw"}>
+                <UnorderedList>
+                    {questions?.map((qn, index) => (
+                        <QuestionItem
+                            qn={qn}
+                            bg={bg}
+                            hoverColor={hoverColor}
+                            key={qn.question}
+                            index={index}
+                            hasSubmit={hasSubmit}
+                        />
+                    ))}
+                </UnorderedList>
+            </Box>
+        </GridItem>
+    );
 };
 export default QuestionList;
