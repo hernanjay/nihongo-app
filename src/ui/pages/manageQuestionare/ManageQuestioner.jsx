227725d27831f06e8cda29bd8a89a169b0e37ecb
Jayson Roda <jayson.roda@awsys-i.com>
2023-12-05 15:12:15 +0800

    Add more functons in Manage Questionaire -Roda

--- src/ui/pages/manageQuestionare/ManageQuestioner.jsx
+++ src/ui/pages/manageQuestionare/ManageQuestioner.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 // import { Link } from "react-router-dom";
 import {
@@ -28,62 +28,74 @@ import {
     Grid,
     Textarea,
     useBoolean,
+    TableContainer,
+    Tfoot,
+    Spacer,
+    useToast,
 } from "@chakra-ui/react";
-import { FiPlusCircle } from "react-icons/fi";
+import { FiCheckCircle, FiPlusCircle, FiTrash2 } from "react-icons/fi";
 import SideBar from "../../components/SideBar";
-import MenuComponent from "../../components/MenuComponent";
 import ThemeColors from "../main/ThemeColors";
 import AddQuestions from "./AddQuestions";
+import QuestionRow from "./QuestionRow";
+import { addQuestions } from "../../../logic/services/apiQuestions";
+import ViewQuestion from "./viewQuestion";
 //   const [display, changeDisplay] = useState("hide");
 function ManageQuestioner() {
+    const toast = useToast();
     const [questions, setQuestions] = useState([]);
-    // const [tableData, setTableData] = useState([]);
-    // const [selectTypeValue, setSelectTypeValue] = useState("");
-    // const [addButtonClicked, setAddButtonClicked] = useState(false);
+    const [isView, setIsView] = useState(false);
+    const [qnPreview, setQnPreview] = useState(null);
+
     const { isOpen, onOpen, onClose } = useDisclosure({
         closeOnOverlayClick: false,
         closeOnEsc: false,
     });
-    // console.log(questions);
-
-    const addTranslationInput = () => {
-        if (translationFields.length < 4) {
-            setTranslationFields([...translationFields, ""]);
-        }
-    };
-
-    const translationInputChange = (index, value) => {
-        const updatedFields = [...translationFields];
-        updatedFields[index] = value;
-        setTranslationFields(updatedFields);
-    };
-
-    const resetFields = () => {
-        setOptions([]);
-        setTranslationFields([]);
-        onClose();
-    };
-
-    const saveBtn = () => {
-        // Combine input fields and translation fields into a single entry
-        // const entry = {
-        //   options: options,
-        //   translation: translationFields,
-        // };
-
-        // Update the table data
-        // setTableData((prevTableData) => [...prevTableData, entry]);
-
-        // Reset input fields
-        setOptions([]);
-        setTranslationFields([]);
-
-        // Close the modal
-        onClose();
-    };
+    console.log(isView);
 
     const [toggle, setToggle] = useBoolean();
     const { bg, fontColor, body } = ThemeColors();
+
+    function handleClearBtn() {
+        setQuestions([]);
+        localStorage.removeItem("questions");
+    }
+
+    async function handleSubmit() {
+        const isAdded = await addQuestions(questions);
+
+        if (isAdded.status) {
+            toast({
+                title: "Questions Added Successfully!",
+                position: "top",
+                status: "success",
+                duration: 3000,
+                isClosable: true,
+            });
+            handleClearBtn();
+        } else {
+            toast({
+                title: "Questions Not Added!",
+                position: "top",
+                status: "error",
+                description: `${isAdded.json.error}`,
+                duration: 3000,
+                isClosable: true,
+            });
+        }
+    }
+
+    function deleteQuestion(i) {
+        const updatedQuestions = questions.filter((qn, index) => index !== i);
+        localStorage.setItem("questions", JSON.stringify(updatedQuestions));
+        setQuestions(updatedQuestions);
+    }
+
+    useEffect(() => {
+        const lsQuestions = JSON.parse(localStorage.getItem("questions"));
+
+        if (lsQuestions) setQuestions(lsQuestions);
+    }, []);
     return (
         <Box>
             <Flex flexDir={"row"} justifyContent={"center"} overflow={"auto"}>
@@ -96,13 +108,7 @@ function ManageQuestioner() {
                     p="5.5rem"
                     flexDir={"column"}
                 >
-                    <MenuComponent />
-
-                    <Flex align="flex-end">
-                        <Heading mt={5} as="h2" size="lg" letterSpacing="tight">
-                            Manage Questionnaires
-                        </Heading>
-                    </Flex>
+                    <Heading mt={5}>Manage Questionnaires</Heading>
                     <Box h="10%" alignSelf="flex-end">
                         <Button
                             leftIcon={<FiPlusCircle />}
@@ -116,49 +122,67 @@ function ManageQuestioner() {
                             onClose={onClose}
                             setQuestions={setQuestions}
                         />
-                    </Box>
-                    <Flex flexDir="column">
-                        <Flex overflow="auto">
-                            <Table variant="unstyled" mt={4}>
-                                <Thead>
-                                    <Tr color={fontColor}>
-                                        <Th>Level</Th>
-                                        <Th>Type</Th>
-                                        <Th>Set</Th>
-                                        <Th>Question</Th>
-                                        <Th>Options</Th>
-                                        <Th>Answer</Th>
-                                        <Th>Option Translate</Th>
-                                        <Th>Question Translate</Th>
-                                    </Tr>
-                                </Thead>
-                                <Tbody>
-                                    <Tr>
-                                        <Td></Td>
-                                    </Tr>
-                                </Tbody>
-                            </Table>
-                        </Flex>
-                    </Flex>
-                    {/* <Flex flexDir="column">
-                        <Flex overflow="auto">
-
-                        </Flex>
-                        <Flex align="center">
-                            <Divider />
-                            <IconButton
-                                icon={display == "show" ? <FiChevronUp /> : <FiChevronDown />}
-                                onClick={() => {
-                                    if (display == "show") {
-                                        changeDisplay("none");
-                                    } else {
-                                        changeDisplay("show");
-                                    }
-                                }}
+                        {isView && (
+                            <ViewQuestion
+                                isView={isView}
+                                setIsView={setIsView}
+                                qn={qnPreview}
                             />
-                            <Divider />
-                        </Flex>
-                    </Flex> */}
+                        )}
+                    </Box>
+                    <TableContainer mt="2rem" overflowY="auto" maxH="60vh">
+                        <Table variant="unstyled">
+                            <Thead position="sticky" top={0} bg={bg} zIndex={2}>
+                                <Tr color={fontColor}>
+                                    <Th>ID</Th>
+                                    <Th>Level</Th>
+                                    <Th>Type</Th>
+                                    <Th>Set</Th>
+                                    <Th>Question</Th>
+                                    <Th>Options</Th>
+                                    <Th>Answer</Th>
+                                    <Th>Option Translate</Th>
+                                    <Th>Question Translate</Th>
+                                    <Th position="sticky" right="0" bg={bg}>
+                                        Action
+                                    </Th>
+                                </Tr>
+                            </Thead>
+                            <Tbody>
+                                {questions.map((question, index) => (
+                                    <QuestionRow
+                                        question={question}
+                                        key={index}
+                                        index={index}
+                                        setIsView={setIsView}
+                                        setQnPreview={setQnPreview}
+                                        deleteQuestion={deleteQuestion}
+                                    />
+                                ))}
+                            </Tbody>
+                        </Table>
+                    </TableContainer>
+                    <Flex mt={5}>
+                        <Button
+                            ms="auto"
+                            bg="red.500"
+                            colorScheme="red"
+                            hidden={questions.length < 1}
+                            me={4}
+                            onClick={handleClearBtn}
+                        >
+                            Delete All
+                        </Button>
+                        <Button
+                            hidden={questions.length < 1}
+                            bg="green.500"
+                            colorScheme="green"
+                            leftIcon={<FiCheckCircle />}
+                            onClick={handleSubmit}
+                        >
+                            Submit
+                        </Button>
+                    </Flex>
                 </Flex>
             </Flex>
         </Box>
--- src/ui/pages/manageQuestionare/ViewQuestion.jsx
+++ src/ui/pages/manageQuestionare/ViewQuestion.jsx
@@ -19,139 +19,15 @@ import {
     Select,
     Textarea,
     Tooltip,
-    useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
-import { useEffect } from "react";
 import { FiPlusCircle, FiTrash2 } from "react-icons/fi";
+import ThemeColors from "../main/ThemeColors";
 
-const AddQuestions = ({ isOpen, onClose, setQuestions }) => {
-    const [qn, setQn] = useState({
-        question: "",
-        options: [],
-        type: "vocab",
-        level: "5",
-        set: "1",
-        answer: "",
-        optionsTranslate: [],
-        questionTranslate: "",
-    });
-
-    const toast = useToast();
-
-    const [hasSubmit, setHasSubmit] = useState(false);
-
-    const optLength = qn.options.length;
-
-    const isErrorSet = qn.set === "";
-    const isErrorQuestion = qn.question === "";
-    const isErrorOptions = optLength === 0 || qn.options.includes("");
-    const isErrorAnswer = qn.answer === "";
-
-    const addOption = () => {
-        if (optLength < 4) {
-            setQn((prevQn) => ({
-                ...prevQn,
-                options: [...prevQn.options, ""],
-                optionsTranslate: [...prevQn.optionsTranslate, ""],
-            }));
-        }
-    };
-
-    const deleteOption = (curIndex) => {
-        const updatedOptions = qn.options.filter(
-            (option, index) => index !== curIndex
-        );
-
-        const updatedOptionsTranslate = qn.optionsTranslate.filter(
-            (optionTranslate, index) => index !== curIndex
-        );
-
-        setQn((prevQn) => ({
-            ...prevQn,
-            options: updatedOptions,
-            optionsTranslate: updatedOptionsTranslate,
-        }));
-    };
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setQn((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
-
-    const handleOptionsChange = (index, value) => {
-        setQn((prevQn) => {
-            const updatedOptions = [...prevQn.options];
-            updatedOptions[index] = value;
-            return {
-                ...prevQn,
-                options: updatedOptions,
-            };
-        });
-    };
-
-    const handleOptionsTranslateChange = (index, value) => {
-        setQn((prevQn) => {
-            const updatedOptions = [...prevQn.optionsTranslate];
-            updatedOptions[index] = value;
-            return {
-                ...prevQn,
-                optionsTranslate: updatedOptions,
-            };
-        });
-    };
-
-    const addQuestion = () => {
-        setHasSubmit(true);
-
-        if (
-            !isErrorSet &&
-            !isErrorQuestion &&
-            !isErrorOptions &&
-            !isErrorAnswer
-        ) {
-            const lsQuestions = JSON.parse(localStorage.getItem("questions"));
-
-            if (!lsQuestions) {
-                console.log(lsQuestions);
-                localStorage.setItem("questions", JSON.stringify([qn]));
-            } else {
-                lsQuestions.push(qn);
-                localStorage.setItem("questions", JSON.stringify(lsQuestions));
-            }
-            setQuestions((prevData) => [...prevData, qn]);
-            resetQn();
-        } else {
-            toast({
-                title: "Questions Not Added!",
-                position: "top",
-                status: "error",
-                description: `Please fill-up required fields`,
-                duration: 3000,
-                isClosable: true,
-            });
-        }
-    };
-
-    function resetQn() {
-        setHasSubmit(false);
-        setQn({
-            question: "",
-            options: [],
-            type: "vocab",
-            level: "5",
-            set: "1",
-            answer: "",
-            optionsTranslate: [],
-            questionTranslate: "",
-        });
-        onClose();
-    }
+const ViewQuestion = ({ isView, setIsView, qn }) => {
+    const { fontColor, border } = ThemeColors();
     return (
-        <Modal isOpen={isOpen} size="3xl" isCloseable={false}>
+        <Modal isOpen={isView} size="3xl" isCloseable={false}>
             <ModalOverlay />
             <ModalContent>
                 <ModalHeader>Questionnaire Form</ModalHeader>
@@ -159,24 +35,24 @@ const AddQuestions = ({ isOpen, onClose, setQuestions }) => {
                     <Grid templateColumns="repeat(3, 1fr)" gap={4}>
                         <FormControl isRequired>
                             <FormLabel>Select Level</FormLabel>
-                            <Select
-                                name="level"
-                                defaultValue="5"
-                                onChange={(e) => handleChange(e)}
-                            >
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
+                            <Select name="level" defaultValue="N5" isReadOnly>
+                                <option>N1</option>
+                                <option>N2</option>
+                                <option>N3</option>
+                                <option>N4</option>
+                                <option>N5</option>
                             </Select>
                         </FormControl>
                         <FormControl isRequired>
                             <FormLabel>Select Type</FormLabel>
                             <Select
                                 name="type"
-                                defaultValue="vocab"
-                                onChange={(e) => handleChange(e)}
+                                value={qn.type}
+                                isDisabled
+                                _disabled={{
+                                    color: "var(--chakra-colors-light.400)",
+                                    borderColor: "var(--chakra-colors-blue)",
+                                }}
                             >
                                 <option value="vocab">Vocab</option>
                                 <option value="grammar">Grammar</option>
@@ -185,55 +61,57 @@ const AddQuestions = ({ isOpen, onClose, setQuestions }) => {
                         </FormControl>
                         <FormControl
                             isRequired
-                            isInvalid={hasSubmit && isErrorSet}
+                            // isInvalid={hasSubmit && isErrorSet}
                         >
                             <FormLabel>Set</FormLabel>
                             <Input
                                 type="number"
                                 name="set"
-                                placeholder="1"
-                                defaultValue="1"
-                                onChange={(e) => handleChange(e)}
+                                value={qn.set}
+                                isReadOnly
+                                // placeholder="1"
+                                // defaultValue="1"
                             />
-                            {isErrorSet && (
+                            {/* {isErrorSet && (
                                 <FormErrorMessage>
                                     Set is required.
                                 </FormErrorMessage>
-                            )}
+                            )} */}
                         </FormControl>
                     </Grid>
                     <FormControl
                         isRequired
                         mt={4}
-                        isInvalid={hasSubmit && isErrorQuestion}
+                        // isInvalid={hasSubmit && isErrorQuestion}
                     >
                         <FormLabel>Question</FormLabel>
                         <Textarea
                             placeholder="Write Question"
                             name="question"
-                            onChange={(e) => handleChange(e)}
+                            value={qn.question}
+                            isReadOnly
                         />
-                        {isErrorQuestion && (
+                        {/* {isErrorQuestion && (
                             <FormErrorMessage>
                                 Question is required.
                             </FormErrorMessage>
-                        )}
+                        )} */}
                     </FormControl>
                     <Box mt={5} mb={5}>
                         <FormControl
                             isRequired
-                            isInvalid={hasSubmit && isErrorOptions}
+                            // isInvalid={hasSubmit && isErrorOptions}
                         >
                             <Grid templateColumns="repeat(3, 1fr)">
                                 <GridItem colSpan={1}>
                                     <Flex>
                                         <FormLabel pt={2}>Options</FormLabel>
-                                        <Tooltip
+                                        {/* <Tooltip
                                             hasArrow
                                             label="Add options and options translate"
                                             openDelay={500}
                                             gutter={10}
-                                            isDisabled={optLength === 4}
+                                            // isDisabled={optLength === 4}
                                         >
                                             <Button
                                                 onClick={addOption}
@@ -242,15 +120,15 @@ const AddQuestions = ({ isOpen, onClose, setQuestions }) => {
                                                 size={"sm"}
                                             >
                                                 Add
-                                            </Button>
-                                        </Tooltip>
+                                            </Button> 
+                                        </Tooltip> */}
                                     </Flex>
                                 </GridItem>
 
                                 <GridItem colSpan={2}>
                                     <FormControl
                                         isRequired
-                                        isInvalid={hasSubmit && isErrorAnswer}
+                                        // isInvalid={hasSubmit && isErrorAnswer}
                                     >
                                         <Flex alignItems="center">
                                             <FormLabel mt={1}>Answer</FormLabel>
@@ -264,11 +142,14 @@ const AddQuestions = ({ isOpen, onClose, setQuestions }) => {
                                             <Select
                                                 size="sm"
                                                 name="answer"
-                                                onChange={(e) =>
-                                                    handleChange(e)
-                                                }
+                                                value={qn.answer}
+                                                isReadOnly
+                                                // onChange={(e) =>
+                                                //     handleChange(e)
+                                                // }
                                                 placeholder="Select Answer"
-                                                isDisabled={isErrorOptions}
+                                                question
+                                                // isDisabled={isErrorOptions}
                                             >
                                                 {qn.options.map(
                                                     (opt, index) => (
@@ -297,28 +178,29 @@ const AddQuestions = ({ isOpen, onClose, setQuestions }) => {
                                             id={`options-${index + 1}`}
                                             value={value}
                                             name="options"
-                                            onChange={(e) => {
-                                                handleOptionsChange(
-                                                    index,
-                                                    e.target.value
-                                                );
-                                            }}
+                                            isReadOnly
+                                            // onChange={(e) => {
+                                            //     handleOptionsChange(
+                                            //         index,
+                                            //         e.target.value
+                                            //     );
+                                            // }}
                                         />
 
-                                        <IconButton
+                                        {/* <IconButton
                                             onClick={() => deleteOption(index)}
                                             icon={<FiTrash2 />}
                                             bg="red.500"
                                             colorScheme="red"
                                             ms="1rem"
-                                        />
+                                        /> */}
                                     </Flex>
                                 ))}
-                                {isErrorOptions && (
+                                {/* {isErrorOptions && (
                                     <FormErrorMessage>
                                         Options is required.
                                     </FormErrorMessage>
-                                )}
+                                )} */}
                             </Box>
                         </FormControl>
                     </Box>
@@ -347,25 +229,18 @@ const AddQuestions = ({ isOpen, onClose, setQuestions }) => {
                                                     }`}
                                                     name="optionsTranslate"
                                                     value={value}
-                                                    onChange={(e) =>
-                                                        handleOptionsTranslateChange(
-                                                            index,
-                                                            e.target.value
-                                                        )
-                                                    }
+                                                    isReadOnly
+                                                    // onChange={(e) =>
+                                                    //     handleOptionsTranslateChange(
+                                                    //         index,
+                                                    //         e.target.value
+                                                    //     )
+                                                    // }
                                                 />
                                             </>
                                         )}
                                     </Flex>
                                 ))}
-
-                                {/* <IconButton
-                                    icon={<FiPlus />}
-                                    onClick={addOptionTranslate}
-                                    isDisabled={
-                                        optTranslateLength === optLength
-                                    }
-                                /> */}
                             </Box>
                         </FormControl>
                     </Box>
@@ -377,7 +252,8 @@ const AddQuestions = ({ isOpen, onClose, setQuestions }) => {
                         <Textarea
                             placeholder="Enter Translation"
                             name="questionTranslate"
-                            onChange={(e) => handleChange(e)}
+                            value={qn.questionTranslate}
+                            isReadOnly
                         />
                     </FormControl>
                 </ModalBody>
@@ -386,21 +262,21 @@ const AddQuestions = ({ isOpen, onClose, setQuestions }) => {
                     <Button
                         colorScheme="blue"
                         mr={3}
-                        onClick={() => addQuestion()}
+                        onClick={() => setIsView(false)}
                     >
-                        Save
+                        Edit
                     </Button>
                     <Button
                         colorScheme="red"
                         onClick={() => {
-                            resetQn();
+                            setIsView(false);
                         }}
                     >
-                        Cancel
+                        Close
                     </Button>
                 </ModalFooter>
             </ModalContent>
         </Modal>
     );
 };
-export default AddQuestions;
+export default ViewQuestion;
