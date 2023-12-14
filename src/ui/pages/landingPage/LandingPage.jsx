5d777c0602e70133be72b4e512dd3e761a68bc84
itsmehernanjay <117618066+hernanjay@users.noreply.github.com>
2023-12-14 12:47:06 +0800

    Made improvements and fixes to landing page

--- src/ui/pages/landingPage/LandingPage.jsx
+++ src/ui/pages/landingPage/LandingPage.jsx
@@ -1,4 +1,12 @@
-import { ExternalLinkIcon, InfoOutlineIcon } from "@chakra-ui/icons";
+import {
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ExternalLinkIcon,
+  InfoOutlineIcon,
+  Search2Icon,
+  SearchIcon,
+} from "@chakra-ui/icons";
 import {
   Grid,
   GridItem,
@@ -9,7 +17,6 @@ import {
   Text,
   Heading,
   Button,
-  useColorModeValue,
   Tabs,
   TabList,
   TabPanels,
@@ -21,30 +28,63 @@ import {
   SimpleGrid,
   Box,
   Container,
+  HStack,
+  Spacer,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Input,
+  InputGroup,
+  IconButton,
+  CardFooter,
+  Tag,
+  TagLabel,
+  VStack,
+  UnorderedList,
+  ListItem,
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import CharacterPracticeTest from "../../components/landingPage/CharacterPracticeTest";
 import KanjiQuestionSample from "../../components/landingPage/KanjiQuestionSample";
 import VocabQuestionSample from "../../components/landingPage/VocabQuestionSample";
 import GrammarQuestionSample from "../../components/landingPage/GrammarQuestionSample";
-import Loader from "../../components/Loader";
-import { useUserContext } from "../../../logic/hooks/user/useUserContext";
 import ThemeColors from "../main/ThemeColors";
 
 export default function LandingPage() {
-  const { body, bg, border, fontColor, success, error, warning, info } =
-    ThemeColors();
-  const element = document.getElementById("sampleQuestions");
+  const { bg, border } = ThemeColors();
   const navigate = useNavigate();
-
-  const { user, isLoading } = useUserContext();
+  const sample = [
+    {
+      id: 1,
+      kanji: "浴びる",
+      hiragana: "あびる",
+      romaji: "abiru",
+      meaning: " to take a shower",
+    },
+    {
+      id: 2,
+      kanji: "危ない",
+      hiragana: "あぶない",
+      romaji: "abunai",
+      meaning: " dangerous, risky",
+    },
+    {
+      id: 3,
+      kanji: "あげる",
+      hiragana: "あげる",
+      romaji: "ageru",
+      meaning: " to give",
+    },
+  ];
 
   return (
-    <>
-      {isLoading && <Loader isLoading={isLoading} />}
+    <Box minH="100vh">
+      <Spacer minH="10vh" />
       <Box
-        h="100vh"
-        overflow="auto"
+        h="90vh"
+        overflowY="auto"
+        overflowX="hidden"
         overscrollBehavior="auto"
         sx={{
           "&::-webkit-scrollbar": {
@@ -58,79 +98,35 @@ export default function LandingPage() {
         }}
       >
         {/* Banner */}
-        <Grid
-          h="100vh"
-          templateRows="repeat(5, 1fr)"
-          templateColumns="repeat(8, 1fr)"
-          gap={4}
-          bg={bg}
-        >
-          <GridItem rowSpan={1} colSpan={8} />
-          <GridItem rowSpan={3} colSpan={1} />
-
-          <GridItem
-            rowSpan={3}
-            colSpan={3}
-            justifyContent="center"
-            alignContent="center"
-          >
-            <Image
-              src="https://i.imgur.com/WtVPZmS.png"
-              alt="pictureForLandingPage"
-            ></Image>
-          </GridItem>
-          <GridItem rowSpan={3} colSpan={3}>
-            <Grid
-              h="100%"
-              templateRows="repeat(3, 1fr)"
-              templateColumns="repeat(1, 1fr)"
-            >
-              <GridItem rowSpan={1} />
-              <GridItem rowSpan={1}>
-                <Card bg={"transparent"} variant={"unstyled"}>
-                  <CardHeader my={"5"}>
-                    <Heading fontSize="3vw">日本語のアプリ</Heading>
-                  </CardHeader>
-                  <CardBody>
-                    <Text fontSize="1vw">
-                      Cras a risus ex. In auctor faucibus dolor sit amet
-                      condimentum. Maecenas sed orci vitae ligula commodo dictum
-                      sit amet quis tellus. Maecenas et viverra lacus. Morbi sit
-                      amet ex libero. Praesent sagittis sapien ac fermentum
-                      laoreet. Mauris semper sem urna, nec viverra ante eleifend
-                      sit amet. Phasellus venenatis sagittis scelerisque.
-                      Quisque elit augue, sodales sit amet bibendum a, tempor
-                      eget mi. Proin finibus lectus vitae nisi congue hendrerit.
-                    </Text>
-                    <Button
-                      my={"5"}
-                      variant={"outline"}
-                      bg={"transparent"}
-                      borderColor={border}
-                      onClick={() => {
-                        const element =
-                          document.getElementById("sampleQuestions");
-                        element.scrollIntoView({
-                          behavior: "smooth",
-                        });
-                      }}
-                    >
-                      Take a Look
-                    </Button>
-                  </CardBody>
-                </Card>
-              </GridItem>
-              <GridItem rowSpan={1} />
-            </Grid>
-          </GridItem>
-
-          <GridItem rowSpan={3} />
-          <GridItem rowSpan={1} colSpan={8} textAlign="center">
-            <Heading mt="5vh" fontSize="3vw">
-              --- UNDER DEVELOPMENT ---
-            </Heading>
-          </GridItem>
-        </Grid>
+        <Box bg={bg} display="flex" minH="100vh" minW="100vw">
+          <Center m="auto">
+            <HStack maxW="70vw">
+              <Image
+                boxSize="50%"
+                p="2.5em"
+                src="https://i.imgur.com/TDC5iyi.png"
+                alt="pictureForLandingPage"
+              />
+              <Card bg={"transparent"} variant={"unstyled"}>
+                <CardHeader my={"5"}>
+                  <Heading fontSize="3vw">日本語のアプリ</Heading>
+                </CardHeader>
+                <CardBody>
+                  <Text fontSize="1vw">
+                    Cras a risus ex. In auctor faucibus dolor sit amet
+                    condimentum. Maecenas sed orci vitae ligula commodo dictum
+                    sit amet quis tellus. Maecenas et viverra lacus. Morbi sit
+                    amet ex libero. Praesent sagittis sapien ac fermentum
+                    laoreet. Mauris semper sem urna, nec viverra ante eleifend
+                    sit amet. Phasellus venenatis sagittis scelerisque. Quisque
+                    elit augue, sodales sit amet bibendum a, tempor eget mi.
+                    Proin finibus lectus vitae nisi congue hendrerit.
+                  </Text>
+                </CardBody>
+              </Card>
+            </HStack>
+          </Center>
+        </Box>
         {/* Banner */}
         <Divider />
         {/* Sample Questions */}
@@ -330,78 +326,114 @@ export default function LandingPage() {
         </Box>
         <Divider />
         {/* Try out Questions Card */}
-        {/* Banner */}
-        <Grid
-          h="100vh"
-          templateRows="repeat(5, 1fr)"
-          templateColumns="repeat(8, 1fr)"
-          gap={4}
-          bg={bg}
-        >
-          <GridItem rowSpan={1} colSpan={8} />
-          <GridItem rowSpan={3} colSpan={1} />
+        {/* Sample Dictionary */}
+        <Center id="sampleQuestions" pt={"10"} pb={"5"} bg={bg}>
+          <Heading fontSize="3vw">Sample Dictionary</Heading>
+        </Center>
+        <Container mt="1.5em" minW="90vw" minH="120vh">
+          <Tabs>
+            <HStack bg={bg} p="0.5em" rounded="lg">
+              <Menu>
+                <MenuButton
+                  minW="5em"
+                  as={Button}
+                  rightIcon={<ChevronDownIcon />}
+                >
+                  All
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>N1</MenuItem>
+                  <MenuItem>N2</MenuItem>
+                  <MenuItem>N3</MenuItem>
+                  <MenuItem>N4</MenuItem>
+                  <MenuItem>N5</MenuItem>
+                </MenuList>
+              </Menu>
+              <InputGroup>
+                <Input variant="flushed" placeholder="Search" />
+                <IconButton icon={<Search2Icon />} />
+              </InputGroup>
+              <HStack minW="15%" ml="2em">
+                <ChevronLeftIcon />
+                <Text mx="2em">1 / 100</Text>
+                <ChevronRightIcon />
+              </HStack>
+              <TabList mr="2em">
+                <Tab minW="5em">Card</Tab>
+                <Tab minW="5em">List</Tab>
+              </TabList>
+            </HStack>
 
-          <GridItem
-            rowSpan={3}
-            colSpan={3}
-            justifyContent="center"
-            alignContent="center"
-          >
-            <Image
-              src="https://i.imgur.com/WtVPZmS.png"
-              alt="pictureForLandingPage"
-            ></Image>
-          </GridItem>
-          <GridItem rowSpan={3} colSpan={3}>
-            <Grid
-              h="100%"
-              templateRows="repeat(3, 1fr)"
-              templateColumns="repeat(1, 1fr)"
-            >
-              <GridItem rowSpan={1} />
-              <GridItem rowSpan={1}>
-                <Card bg={"transparent"} variant={"unstyled"}>
-                  <CardHeader my={"5"}>
-                    <Heading fontSize="3vw">日本語のアプリ</Heading>
-                  </CardHeader>
-                  <CardBody>
-                    <Text fontSize="1vw">
-                      Cras a risus ex. In auctor faucibus dolor sit amet
-                      condimentum. Maecenas sed orci vitae ligula commodo dictum
-                      sit amet quis tellus. Maecenas et viverra lacus. Morbi sit
-                      amet ex libero. Praesent sagittis sapien ac fermentum
-                      laoreet. Mauris semper sem urna, nec viverra ante eleifend
-                      sit amet. Phasellus venenatis sagittis scelerisque.
-                      Quisque elit augue, sodales sit amet bibendum a, tempor
-                      eget mi. Proin finibus lectus vitae nisi congue hendrerit.
-                    </Text>
-                    <Button
-                      my={"5"}
-                      variant={"outline"}
-                      bg={"transparent"}
-                      borderColor={border}
-                      onClick={() => {
-                        element.scrollIntoView({
-                          behavior: "smooth",
-                          block: "center",
-                          inline: "nearest",
-                        });
-                      }}
-                    >
-                      Take a Look
-                    </Button>
-                  </CardBody>
-                </Card>
+            <Grid templateColumns="repeat(3, 1fr)" gap={5} mt="2.5em">
+              <GridItem colSpan={1}>
+                {sample.map((obj) => {
+                  return (
+                    <Card bg={bg} borderRadius="lg" mb="1.5em">
+                      <CardBody>
+                        <HStack>
+                          <Spacer />
+                          <Text color="gray.400">{`${obj.hiragana}・${obj.romaji}`}</Text>
+                        </HStack>
+                        <Heading
+                          py=".75em"
+                          textAlign="center"
+                          fontWeight="normal"
+                        >
+                          {obj.kanji}
+                        </Heading>
+                      </CardBody>
+                      <CardFooter>
+                        <Text color="gray.400">{`- ${obj.meaning}`}</Text>
+                      </CardFooter>
+                    </Card>
+                  );
+                })}
               </GridItem>
-              <GridItem rowSpan={1} />
+              <GridItem colSpan={2}>
+                <Container px="2.5em" minW="90%" bg={bg} rounded="lg" h="100vh">
+                  <HStack>
+                    <Tag
+                      bg={bg}
+                      my="2.5em"
+                      ml="2.5em"
+                      borderWidth="1px"
+                      borderColor={border}
+                      p="1em"
+                    >
+                      <TagLabel fontSize="3.5em">{sample[0].kanji}</TagLabel>
+                    </Tag>
+                    <Box ml="1em">
+                      <Text>Reading:</Text>
+                      <Text>{sample[0].hiragana}</Text>
+                      <Text>{sample[0].romaji}</Text>
+                    </Box>
+                  </HStack>
+                  <UnorderedList>
+                    {sample.map((val) => {
+                      return (
+                        <ListItem>
+                          <Text color="gray.400">
+                            決して譲らない, 失望をさせない,
+                            決して置去りにはしない
+                          </Text>
+                          <Text my="0.25em" color="gray.400">
+                            Sed ut perspiciatis, unde omnis iste natus error sit
+                            voluptatem accusantium doloremque laudantium,
+                          </Text>
+                          <Divider borderColor={border} mb="1.5em" />
+                        </ListItem>
+                      );
+                    })}
+                  </UnorderedList>
+                </Container>
+              </GridItem>
             </Grid>
-          </GridItem>
+          </Tabs>
+        </Container>
+        {/* Sample Dictionary */}
 
-          <GridItem rowSpan={3} />
-          <GridItem rowSpan={1} colSpan={8} />
-        </Grid>
-        {/* Banner */}
+        <Box minH="10vh" bg={bg}></Box>
       </Box>
-    </>
+    </Box>
   );
 }
